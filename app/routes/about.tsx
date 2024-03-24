c89f926b54e094f6cafba9b2cb932f19b3b517a1
Matt Mazzola <mattmazzola@live.com>
2024-03-23 17:04:12 -0700

    Add data from client loader

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,5 +1,18 @@
+import { ClientLoaderFunctionArgs, useLoaderData } from "@remix-run/react"
+
+export function clientLoader({ }: ClientLoaderFunctionArgs) {
+    return {
+        message: "Data from Client Loader"
+    }
+}
+
 export default function About() {
+    const loaderData = useLoaderData<typeof clientLoader>()
+
     return (
-        <h1>About</h1>
+        <>
+            <h1>About</h1>
+            <p>{loaderData.message}</p>
+        </>
     )
 }
